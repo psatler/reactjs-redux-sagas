8037d7e6bba4e5a803d456045f55633f812e568c
psatler <pablosatler@gmail.com>
2019-07-12 23:10:58 -0300

    style: add css for the Cart component
    - use baseline with display flex for the Total component

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,5 +1,74 @@
 import React from 'react';
+import {
+    MdAddCircleOutline,
+    MdRemoveCircleOutline,
+    MdDelete,
+} from 'react-icons/md';
+
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-    return <div>Cart component</div>;
+    return (
+        <Container>
+            <ProductTable>
+                <thead>
+                    <tr>
+                        <th />
+                        <th>PRODUTO</th>
+                        <th>QTD</th>
+                        <th>SUBTOTAL</th>
+                        <th />
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <img
+                                src="https://static.netshoes.com.br/produtos/tenis-vr-caminhada-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_detalhe2.jpg?resize=326:*"
+                                alt="Tenis"
+                            />
+                        </td>
+                        <td>
+                            <strong>Tenis muito massa</strong>
+                            <span>R$129,90</span>
+                        </td>
+                        <td>
+                            <div>
+                                <button type="button">
+                                    <MdRemoveCircleOutline
+                                        size={20}
+                                        color="#7159c1"
+                                    />
+                                </button>
+                                <input type="number" readOnly value={2} />
+                                <button type="button">
+                                    <MdAddCircleOutline
+                                        size={20}
+                                        color="#7159c1"
+                                    />
+                                </button>
+                            </div>
+                        </td>
+                        <td>
+                            <strong>R$258,80</strong>
+                        </td>
+                        <td>
+                            <button type="button">
+                                <MdDelete size={20} color="#7159c1" />
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </ProductTable>
+
+            <footer>
+                <button type="button">Finalizar pedido</button>
+
+                <Total>
+                    <span>TOTAL</span>
+                    <strong>R$1920,28</strong>
+                </Total>
+            </footer>
+        </Container>
+    );
 }
