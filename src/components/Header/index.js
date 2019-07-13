81b0ecf9bac966c97ed0c12626574afb574705d4
psatler <pablosatler@gmail.com>
2019-07-13 13:12:23 -0300

    connect header component to reducer and update number of items in cart

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { MdShoppingBasket } from 'react-icons/md';
 import { Container, Cart } from './styles';
 
 import logo from '../../assets/images/logo.png';
 
-export default function Header() {
+// cart is coming from mapStateToProps
+function Header({ cartSize }) {
     return (
         <Container>
             <Link to="/">
@@ -15,10 +17,16 @@ export default function Header() {
             <Cart to="/cart">
                 <div>
                     <strong>My cart</strong>
-                    <span>3 items </span>
+                    <span>{cartSize} items </span>
                 </div>
                 <MdShoppingBasket size={36} color="#FFF" />
             </Cart>
         </Container>
     );
 }
+
+const mapStateToProps = state => ({
+    cartSize: state.cart.length,
+});
+
+export default connect(mapStateToProps)(Header);
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { MdAddShoppingCart } from 'react-icons/md';
 import api from '../../services/api';
 import { formatPrice } from '../../util/format';
 
 import { ProductList } from './styles';
 
-export default class Home extends Component {
+class Home extends Component {
     state = {
         products: [],
     };
@@ -23,6 +24,14 @@ export default class Home extends Component {
         });
     }
 
+    handleAddProduct = product => {
+        const { dispatch } = this.props;
+        dispatch({
+            type: 'ADD_TO_CART',
+            product,
+        });
+    };
+
     render() {
         const { products } = this.state;
         return (
@@ -33,7 +42,10 @@ export default class Home extends Component {
                         <strong>{product.title}</strong>
                         <span>{product.priceFormatted}</span>
 
-                        <button type="button">
+                        <button
+                            type="button"
+                            onClick={() => this.handleAddProduct(product)}
+                        >
                             <div>
                                 <MdAddShoppingCart size={16} color="#FFF" /> 3
                                 <span>Adicionar ao carrinho</span>
@@ -45,3 +57,5 @@ export default class Home extends Component {
         );
     }
 }
+
+export default connect()(Home);
