c7bfef9387447b1959a85b61649f0efefa737a55
psatler <pablosatler@gmail.com>
2019-07-13 15:41:24 -0300

    chore: install immerjs for add mutable methods to redux
    - using immerjs in the reduce. It produces an immutable state even when using mutable methods.

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,13 +1,29 @@
+import produce from 'immer'; // https://github.com/immerjs/immer
+
 export default function cart(state = [], action) {
     switch (action.type) {
         case 'ADD_TO_CART':
-            return [
-                ...state,
-                {
-                    ...action.product,
-                    amount: 1,
-                },
-            ];
+            return produce(state, draft => {
+                const productIndex = draft.findIndex(
+                    p => p.id === action.product.id
+                );
+                if (productIndex >= 0) {
+                    draft[productIndex].amount += 1;
+                } else {
+                    // product not yet in the cart, so amount equals to 1
+                    draft.push({
+                        ...action.product,
+                        amount: 1,
+                    });
+                }
+            });
+        // return [
+        //     ...state,
+        //     {
+        //         ...action.product,
+        //         amount: 1,
+        //     },
+        // ];
 
         default:
             return state;
