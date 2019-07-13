add42c653575689a53f748ae585ece57f00a98e4
psatler <pablosatler@gmail.com>
2019-07-12 22:25:34 -0300

    style: add css to Home component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,106 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
 
 export default function Home() {
-    return <div>Home component</div>;
+    return (
+        <ProductList>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-vr-caminhada-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_detalhe2.jpg?resize=326:*"
+                    alt="Tenis"
+                />
+                <strong>
+                    Very cool sneackerVery cool sneackerVery cool sneackerVery
+                    cool sneackerVery cool sneackerVery cool sneackerVery cool
+                    sneackerVery cool sneackerVery cool sneackerVery cool
+                    sneacker
+                </strong>
+                <span>R$129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                        <span>Adicionar ao carrinho</span>
+                    </div>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-vr-caminhada-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_detalhe2.jpg?resize=326:*"
+                    alt="Tenis"
+                />
+                <strong>Very cool sneacker</strong>
+                <span>R$129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                        <span>Adicionar ao carrinho</span>
+                    </div>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-vr-caminhada-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_detalhe2.jpg?resize=326:*"
+                    alt="Tenis"
+                />
+                <strong>Very cool sneacker</strong>
+                <span>R$129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                        <span>Adicionar ao carrinho</span>
+                    </div>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-vr-caminhada-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_detalhe2.jpg?resize=326:*"
+                    alt="Tenis"
+                />
+                <strong>Very cool sneacker</strong>
+                <span>R$129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                        <span>Adicionar ao carrinho</span>
+                    </div>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-vr-caminhada-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_detalhe2.jpg?resize=326:*"
+                    alt="Tenis"
+                />
+                <strong>Very cool sneacker</strong>
+                <span>R$129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                        <span>Adicionar ao carrinho</span>
+                    </div>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-vr-caminhada-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_detalhe2.jpg?resize=326:*"
+                    alt="Tenis"
+                />
+                <strong>Very cool sneacker</strong>
+                <span>R$129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                        <span>Adicionar ao carrinho</span>
+                    </div>
+                </button>
+            </li>
+        </ProductList>
+    );
 }
