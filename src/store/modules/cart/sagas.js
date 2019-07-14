71d9fc35c0c8f95d076f5649852ef8f1507b70d6
psatler <pablosatler@gmail.com>
2019-07-14 17:23:02 -0300

    feat: perform stock check to see if product is available to be sold
    - we perform another call to api to get the stock amount

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -11,9 +11,20 @@ function* addToCart({ id }) {
         state.cart.find(p => p.id === id)
     );
 
+    // checking the stock amount
+    const stock = yield call(api.get, `/stock/${id}`);
+    const stockAmount = stock.data.amount;
+    // if exists, we use the current amount, otherwise, it's zero because it's not on cart yet
+    const currentAmount = productExists ? productExists.amount : 0;
+
+    const amount = currentAmount + 1; // we update the amount
+
+    if (amount > stockAmount) {
+        console.tron.warn(`Out of stock`);
+        return;
+    }
+
     if (productExists) {
-        // we update the amount
-        const amount = productExists.amount + 1;
         yield put(updateAmount(id, amount));
     } else {
         const response = yield call(api.get, `/products/${id}`);
