1b6bcd9ad84a910585297dfed2b4293722c18009
psatler <pablosatler@gmail.com>
2019-07-14 13:49:34 -0300

    feat: calculate totals on mapStateToProps of Cart component

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -7,11 +7,21 @@ import {
     MdDelete,
 } from 'react-icons/md';
 
+import { formatPrice } from '../../util/format';
+
 import * as CartActions from '../../store/modules/cart/actions';
 
 import { Container, ProductTable, Total } from './styles';
 
-function Cart({ cart, removeFromCart }) {
+function Cart({ cart, total, removeFromCart, updateAmount }) {
+    function increment(product) {
+        updateAmount(product.id, product.amount + 1);
+    }
+
+    function decrement(product) {
+        updateAmount(product.id, product.amount - 1);
+    }
+
     return (
         <Container>
             <ProductTable>
@@ -36,7 +46,10 @@ function Cart({ cart, removeFromCart }) {
                             </td>
                             <td>
                                 <div>
-                                    <button type="button">
+                                    <button
+                                        type="button"
+                                        onClick={() => decrement(product)}
+                                    >
                                         <MdRemoveCircleOutline
                                             size={20}
                                             color="#7159c1"
@@ -47,7 +60,10 @@ function Cart({ cart, removeFromCart }) {
                                         readOnly
                                         value={product.amount}
                                     />
-                                    <button type="button">
+                                    <button
+                                        type="button"
+                                        onClick={() => increment(product)}
+                                    >
                                         <MdAddCircleOutline
                                             size={20}
                                             color="#7159c1"
@@ -56,7 +72,7 @@ function Cart({ cart, removeFromCart }) {
                                 </div>
                             </td>
                             <td>
-                                <strong>R$258,80</strong>
+                                <strong>{product.subTotal}</strong>
                             </td>
                             <td>
                                 <button
@@ -76,7 +92,7 @@ function Cart({ cart, removeFromCart }) {
 
                 <Total>
                     <span>TOTAL</span>
-                    <strong>R$1920,28</strong>
+                    <strong>{total}</strong>
                 </Total>
             </footer>
         </Container>
@@ -84,7 +100,16 @@ function Cart({ cart, removeFromCart }) {
 }
 
 const mapStateToProps = state => ({
-    cart: state.cart,
+    cart: state.cart.map(product => ({
+        ...product,
+        subTotal: formatPrice(product.price * product.amount),
+    })),
+    // reduce: accumulator, currentValue
+    total: formatPrice(
+        state.cart.reduce((total, product) => {
+            return total + product.price * product.amount;
+        }, 0)
+    ), // initiate with value of zero
 });
 
 const mapDispatchToProps = dispatch =>
